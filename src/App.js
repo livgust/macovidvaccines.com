14b8407c3eef0182391f39971acb69dfe8316a17
Matthew Cardarelli <mattrc7@gmx.com>
2021-02-15 12:59:21 -0500

    Improve HTML semantics of a couple key elements

--- src/App.js
+++ src/App.js
@@ -6,36 +6,48 @@ import Menu from "./components/Menu";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-	main: {
-		padding: theme.spacing(2),
-	},
-	heading: {
-		"text-align": "center",
-	},
+    main: {
+        padding: theme.spacing(2),
+    },
+    heading: {
+        "text-align": "center",
+    },
 }));
 
 function App() {
-	const classes = useStyles();
-	return (
-		<>
-			<Menu />
-			<div className={classes.main}>
-				<Grid container justify="center" spacing={3}>
-					<Grid item xs={1} sm={2}></Grid>
-					<Grid item xs={10} sm={8}>
-						<h1 className={classes.heading}>MA Covid Vaccine Appointments</h1>
-						<CovidAppointmentTable />
-						<Typography variant="caption" display="block" gutterBottom>
-						This site is not affiliated with or endorsed by the Commonwealth of Massachusetts.<br/>
-This site is for informational purposes only.   Not all vaccination locations are tracked and the information may not be complete or accurate.<br/>
-							Copyright &#169; {new Date().getFullYear()} Olivia Adams. All rights reserved.
-						</Typography>
-					</Grid>
-					<Grid item xs={1} sm={2}></Grid>
-				</Grid>
-			</div>
-		</>
-	);
+    const classes = useStyles();
+    return (
+        <>
+            <Menu />
+            <main className={classes.main}>
+                <Grid container justify="center" spacing={3}>
+                    <Grid item xs={1} sm={2}></Grid>
+                    <Grid item xs={10} sm={8}>
+                        <h1 className={classes.heading}>
+                            MA Covid Vaccine Appointments
+                        </h1>
+                        <CovidAppointmentTable />
+                        <Typography
+                            variant="caption"
+                            display="block"
+                            gutterBottom
+                        >
+                            This site is not affiliated with or endorsed by the
+                            Commonwealth of Massachusetts.
+                            <br />
+                            This site is for informational purposes only. Not
+                            all vaccination locations are tracked and the
+                            information may not be complete or accurate.
+                            <br />
+                            Copyright &#169; {new Date().getFullYear()} Olivia
+                            Adams. All rights reserved.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={1} sm={2}></Grid>
+                </Grid>
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,130 +13,136 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-	},
-	buttonText: {
-		color: theme.palette.primary.contrastText,
-	},
-	title: {
-		flexGrow: 1,
-	},
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    buttonText: {
+        color: theme.palette.primary.contrastText,
+    },
+    title: {
+        flexGrow: 1,
+    },
 }));
 
 export default function ButtonAppBar() {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const [aboutOpen, setAboutOpen] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [aboutOpen, setAboutOpen] = React.useState(false);
 
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-	return (
-		<div className={classes.root}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="menu"
-						aria-controls="simple-menu"
-						aria-haspopup="true"
-						onClick={handleClick}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Button
-						className={classes.buttonText}
-						href="https://www.gofundme.com/f/wwwmacovidvaccinescom?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Contribute
-					</Button>
-				</Toolbar>
-			</AppBar>
-			<Menu
-				id="simple-menu"
-				anchorEl={anchorEl}
-				keepMounted
-				open={Boolean(anchorEl)}
-				onClose={handleClose}
-			>
-				<MenuItem
-					onClick={() => {
-						handleClose();
-						setAboutOpen(true);
-					}}
-				>
-					About
-				</MenuItem>
-			</Menu>
-			<AboutDialog open={aboutOpen} onClose={() => setAboutOpen(false)} />
-		</div>
-	);
+    return (
+        <header className={classes.root}>
+            <AppBar position="static">
+                <Toolbar component="nav">
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                        aria-controls="simple-menu"
+                        aria-haspopup="true"
+                        onClick={handleClick}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Button
+                        className={classes.buttonText}
+                        href="https://www.gofundme.com/f/wwwmacovidvaccinescom?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Contribute
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem
+                    onClick={() => {
+                        handleClose();
+                        setAboutOpen(true);
+                    }}
+                >
+                    About
+                </MenuItem>
+            </Menu>
+            <AboutDialog open={aboutOpen} onClose={() => setAboutOpen(false)} />
+        </header>
+    );
 }
 
 function AboutDialog(props) {
-	return (
-		<Dialog {...props}>
-			<DialogTitle id="about-dialog-title">{"About this Website"}</DialogTitle>
-			<DialogContent>
-				<DialogContentText id="about-dialog-description">
-					<p>
-						This website was created by{" "}
-						<a
-							href="http://www.oliviaadams.dev"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Olivia Adams
-						</a>
-						.
-					</p>
-					<p>
-						This website scrapes data from other websites every 5 minutes and
-						tells you what places currently are advertising available
-						appointments for COVID vaccines. It is YOUR responsibility to verify
-						that you are eligible before signing up. For more information, click{" "}
-						<a
-							href="http://www.mass.gov/covidvaccine"
-							target="_blank"
-							rel="noreferrer"
-						>
-							here
-						</a>
-						.
-					</p>
-					<p>
-						Currently, information is gathered from/for:
-						<ul>
-							<li>maimmunizations.org (many locations)</li>
-							<li>Hannaford (5 locations)</li>
-							<li>UMass Amherst</li>
-							<li>Springfield: Eastfield Mall</li>
-							<li>Danvers: Doubletree Hotel</li>
-							<li>Arlington: Family Practice Group, PC</li>
-						</ul>
-					</p>
-					<p>
-						I'm working as fast as I can to gather more information from other
-						sources. To contact me,{" "}
-						<a href="mailto:macovidvaccines@gmail.com">send an email</a>. I'll
-						try to get back to you but I'm getting a lot of outreach!
-					</p>
-				</DialogContentText>
-			</DialogContent>
-		</Dialog>
-	);
+    return (
+        <Dialog {...props}>
+            <DialogTitle id="about-dialog-title">
+                {"About this Website"}
+            </DialogTitle>
+            <DialogContent>
+                <DialogContentText id="about-dialog-description">
+                    <p>
+                        This website was created by{" "}
+                        <a
+                            href="http://www.oliviaadams.dev"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Olivia Adams
+                        </a>
+                        .
+                    </p>
+                    <p>
+                        This website scrapes data from other websites every 5
+                        minutes and tells you what places currently are
+                        advertising available appointments for COVID vaccines.
+                        It is YOUR responsibility to verify that you are
+                        eligible before signing up. For more information, click{" "}
+                        <a
+                            href="http://www.mass.gov/covidvaccine"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            here
+                        </a>
+                        .
+                    </p>
+                    <p>
+                        Currently, information is gathered from/for:
+                        <ul>
+                            <li>maimmunizations.org (many locations)</li>
+                            <li>Hannaford (5 locations)</li>
+                            <li>UMass Amherst</li>
+                            <li>Springfield: Eastfield Mall</li>
+                            <li>Danvers: Doubletree Hotel</li>
+                            <li>Arlington: Family Practice Group, PC</li>
+                        </ul>
+                    </p>
+                    <p>
+                        I'm working as fast as I can to gather more information
+                        from other sources. To contact me,{" "}
+                        <a href="mailto:macovidvaccines@gmail.com">
+                            send an email
+                        </a>
+                        . I'll try to get back to you but I'm getting a lot of
+                        outreach!
+                    </p>
+                </DialogContentText>
+            </DialogContent>
+        </Dialog>
+    );
 }
