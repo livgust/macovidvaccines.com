73e14916419255c9922266522a86867225496bdd
Thomas Ricci <1tomricci@gmail.com>
2021-02-16 19:36:20 -0500

    finished markdown parsing

--- src/components/MoreInformation.js
+++ src/components/MoreInformation.js
@@ -5,114 +5,153 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  accordion: {
-    "margin-top": theme.spacing(2),
-    "margin-bottom": theme.spacing(2),
-    ":last-child": { "margin-bottom": theme.spacing(2) },
-  },
-  buttonText: {
-    ...theme.typography.button,
-  },
-  extraDataContainer: {
-    display: "block",
-  },
-  extraData: {
-    display: "block",
-    "padding-bottom": theme.spacing(1),
-  },
-  formattedExtraData: {
-    color: "#F50057",
-  },
+    accordion: {
+        "margin-top": theme.spacing(2),
+        "margin-bottom": theme.spacing(2),
+        ":last-child": { "margin-bottom": theme.spacing(2) },
+    },
+    buttonText: {
+        ...theme.typography.button,
+    },
+    extraDataContainer: {
+        display: "block",
+    },
+    extraData: {
+        display: "block",
+        "padding-bottom": theme.spacing(1),
+    },
+    formattedExtraData: {
+        color: "#F50057",
+    },
 }));
 
 export default function MoreInformation({ entry }) {
-  const classes = useStyles();
-  if (entry.extraData) {
-    //reggie lewis uses markdown but not for emphasis so it skips that site
-    if (entry.location !== "Reggie Lewis State Track Athletic Ctr") {
-      const currentData = entry.extraData["Additional Information"];
-      if (currentData) {
-        let splitDataEmStr = currentData.split("***");
-        let i = 0;
-        let newDataEmStr = splitDataEmStr[0];
-        let isEmStr = false;
-        for(i = 1; i < splitDataEmStr.length; i++) {
-          if (isEmStr) {
-            newDataEmStr = newDataEmStr + "</span></em>" + splitDataEmStr[i];
-            isEmStr = false;
-          } else {
-            newDataEmStr =
-              newDataEmStr +
-              '<em><span class="formattedExtraData">' +
-              splitDataEmStr[i];
-            isEmStr = true;
-          }
-        }
-        let splitDataStr = newDataEmStr.split("**");
-        let newDataStr = splitDataStr[0];
-        let isStr = false;
-        for(i = 1; i < splitDataStr.length; i++) {
-          if (isStr) {
-            newDataStr = newDataStr + "</span>" + splitDataStr[i];
-            isStr = false;
-          } else {
-            newDataStr =
-              newDataStr +
-              '<span class="formattedExtraData">' +
-              splitDataStr[i];
-            isStr = true;
-          }
-        }
-        let splitDataEm = newDataStr.split("*");
-        let newDataEm = splitDataEm[0];
-        let isEm = false;
-        for(i = 1; i < splitDataEm.length; i++) {
-          if (isEm) {
-            newDataEm = newDataEm + "</em>" + splitDataEm[i];
-            isEm = false;
-          } else {
-            newDataEm = newDataEm + "<em>" + splitDataEm[i];
-            isEm = true;
-          }
-        }
-        entry.extraData["Additional Information"] = newDataEm;
-      }
+    const classes = useStyles();
+    if (entry.extraData) {
     }
-  }
-  return (
-    <Accordion className={classes.accordion}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        className={classes.buttonText}
-      >
-        More Information
-      </AccordionSummary>
-      <AccordionDetails className={classes.extraDataContainer}>
-        <div className={classes.extraData}>
-          <b>Address:</b> {entry.streetAddress}, {entry.city}, MA {entry.zip}
-        </div>
-        <ExtraData data={entry.extraData} />
-      </AccordionDetails>
-    </Accordion>
-  );
+    return (
+        <Accordion className={classes.accordion}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                className={classes.buttonText}
+            >
+                More Information
+            </AccordionSummary>
+            <AccordionDetails className={classes.extraDataContainer}>
+                <div className={classes.extraData}>
+                    <b>Address:</b> {entry.streetAddress}, {entry.city}, MA{" "}
+                    {entry.zip}
+                </div>
+                <ExtraData data={entry.extraData} />
+            </AccordionDetails>
+        </Accordion>
+    );
 }
 
 function ExtraData({ data }) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  if (!data) {
-    return null;
-  } else if (typeof data == "string") {
-    return <div>data</div>;
-  } else if (typeof data == "object") {
-    let elements = [];
-    for (const key in data) {
-      elements.push(
-        <div className={classes.extraData}>
-          <b>{key}:</b> {data[key]}
-        </div>
-      );
+    if (!data) {
+        return null;
+    } else if (typeof data == "string") {
+        return <div>data</div>;
+    } else if (typeof data == "object") {
+        let elements = [];
+        for (const key in data) {
+            //formats additional information instead of just adding it to the more information section
+            if (key === "Additional Information") {
+                const currentData = data[key];
+                if (currentData) {
+                    var finalData = parseMD(currentData);
+                    //adds the parsed data as straight html
+                    elements.push(
+                        <div
+                            className={classes.extraData}
+                            dangerouslySetInnerHTML={{
+                                __html: `<b>${key}:</b> ${finalData}`,
+                            }}
+                        />
+                    );
+                }
+            } else {
+                elements.push(
+                    <div className={classes.extraData}>
+                        <b>{key}:</b> {data[key]}
+                    </div>
+                );
+            }
+        }
+        return elements;
     }
-    return elements;
-  }
+}
+
+function parseMD(currentData) {
+    //encodes angle brackets to escape data
+    let workingData1;
+    let workingData2;
+    let indexOfLAB = currentData.indexOf("<");
+    let indexOfRAB = currentData.indexOf(">");
+    if (indexOfLAB !== -1) {
+        workingData1 = currentData.replaceAll("<", "&#60;");
+    } else {
+        workingData1 = currentData;
+    }
+    if (indexOfRAB !== -1) {
+        workingData2 = workingData1.replaceAll(">", "&#62;");
+    } else {
+        workingData2 = workingData1;
+    }
+    //parses bold italics first, splitting data at each "***"
+    let splitDataEmStr = workingData2.split("***");
+    let newDataEmStr = splitDataEmStr[0];
+    let isEmStr = false;
+    //iterates through the split data
+    //uses em to italicize and span with the secondary color for bolding (went with a color change since the titles were bold)
+    let i = 0;
+    for (i = 1; i < splitDataEmStr.length; i++) {
+        if (isEmStr) {
+            newDataEmStr = newDataEmStr + "</span></em>" + splitDataEmStr[i];
+            isEmStr = false;
+        } else {
+            newDataEmStr =
+                newDataEmStr +
+                '<em><span style="color: #F50057">' +
+                splitDataEmStr[i];
+            isEmStr = true;
+        }
+    }
+    //parses bold next, splitting data at each "**"
+    let splitDataStr = newDataEmStr.split("**");
+    let newDataStr = splitDataStr[0];
+    let isStr = false;
+    //iterates through the split data
+    //same formatting rules as 85
+    for (i = 1; i < splitDataStr.length; i++) {
+        if (isStr) {
+            newDataStr = newDataStr + "</span>" + splitDataStr[i];
+            isStr = false;
+        } else {
+            newDataStr =
+                newDataStr +
+                '<span class="formattedExtraData">' +
+                splitDataStr[i];
+            isStr = true;
+        }
+    }
+    //parses italics last, splitting data at each "*"
+    let splitDataEm = newDataStr.split("*");
+    let newDataEm = splitDataEm[0];
+    let isEm = false;
+    //iterates through the split data
+    //same formatting rules as 85
+    for (i = 1; i < splitDataEm.length; i++) {
+        if (isEm) {
+            newDataEm = newDataEm + "</em>" + splitDataEm[i];
+            isEm = false;
+        } else {
+            newDataEm = newDataEm + "<em>" + splitDataEm[i];
+            isEm = true;
+        }
+    }
+    return newDataEm;
 }
