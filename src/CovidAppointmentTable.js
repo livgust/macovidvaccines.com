025a482945b7271aa2eb43e4f49f7b4957a65ff3
Derek Lee <dldereklee@gmail.com>
2021-02-16 19:00:30 -0500

    Apply prettier

--- src/CovidAppointmentTable.js
+++ src/CovidAppointmentTable.js
@@ -6,31 +6,31 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
-import Button from '@material-ui/core/Button';
-import RoomOutlined from '@material-ui/icons/RoomOutlined';
-import GpsFixedIcon from '@material-ui/icons/GpsFixed';
-import TextField from "@material-ui/core/TextField"
-import getDistance from 'geolib/es/getDistance';
+import Button from "@material-ui/core/Button";
+import RoomOutlined from "@material-ui/icons/RoomOutlined";
+import GpsFixedIcon from "@material-ui/icons/GpsFixed";
+import TextField from "@material-ui/core/TextField";
+import getDistance from "geolib/es/getDistance";
 
 import Availability from "./components/Availability";
 import SignUpLink from "./components/SignUpLink";
 import MoreInformation from "./components/MoreInformation";
 
 export function transformData(data) {
-	return data.map((entry, index) => {
-		return {
-			key: index,
-			location: entry.name,
-			streetAddress: entry.street,
-			city: entry.city,
-			zip: entry.zip,
-			distance: entry.distance,
-			hasAppointments: entry.hasAvailability,
-			appointmentData: entry.availability || null,
-			signUpLink: entry.signUpLink || null,
-			extraData: entry.extraData || null,
-		};
-	});
+    return data.map((entry, index) => {
+        return {
+            key: index,
+            location: entry.name,
+            streetAddress: entry.street,
+            city: entry.city,
+            zip: entry.zip,
+            distance: entry.distance,
+            hasAppointments: entry.hasAvailability,
+            appointmentData: entry.availability || null,
+            signUpLink: entry.signUpLink || null,
+            extraData: entry.extraData || null,
+        };
+    });
 }
 
 export function sortAndFilterData(
@@ -54,67 +54,75 @@ export function sortAndFilterData(
 }
 
 const useStyles = makeStyles((theme) => ({
-	cardBox: {
-		"padding-top": theme.spacing(2),
-		"padding-bottom": theme.spacing(2),
-	},
-	filterButtons: {
-		"margin-top": theme.spacing(1),
-		"margin-bottom": theme.spacing(1),
-		"margin-left": theme.spacing(1)
-	},
-	locationsNotFound: {
-		"margin": theme.spacing(1, "auto")
-	},
-	notFoundBox: {
-		"display": "grid"
-	}
+    cardBox: {
+        "padding-top": theme.spacing(2),
+        "padding-bottom": theme.spacing(2),
+    },
+    filterButtons: {
+        "margin-top": theme.spacing(1),
+        "margin-bottom": theme.spacing(1),
+        "margin-left": theme.spacing(1),
+    },
+    locationsNotFound: {
+        margin: theme.spacing(1, "auto"),
+    },
+    notFoundBox: {
+        display: "grid",
+    },
 }));
 
-
-
 export default function CovidAppointmentTable() {
-	const classes = useStyles();
-	const [loading, setLoading] = useState(false)
-	const [ready, setReady] = useState(false);
-	const [errorMessage, setErrorMessage] = useState();
-	const [currentLocation, setCurrentLocation] = useState({});
-	const [zipcode, setZipcode] = useState("")
-	const [data, setData] = useState([]);
-	const [sortInfo, setSortInfo] = useState({
-		sortKey: "hasAppointments",
-		sortAsc: false,
-	});
+    const classes = useStyles();
+    const [loading, setLoading] = useState(false);
+    const [ready, setReady] = useState(false);
+    const [errorMessage, setErrorMessage] = useState();
+    const [currentLocation, setCurrentLocation] = useState({});
+    const [zipcode, setZipcode] = useState("");
+    const [data, setData] = useState([]);
+    const [sortInfo, setSortInfo] = useState({
+        sortKey: "hasAppointments",
+        sortAsc: false,
+    });
 
     const [onlyShowAvailable, setOnlyShowAvailable] = useState(true);
 
-	useEffect(() => {
-		resetData()
-	}, []);
+    useEffect(() => {
+        resetData();
+    }, []);
 
-	useEffect(()=> {
-		setLoading(true)
-		setData(data => data.filter(entry => entry.latitude && entry.longitude)
-			.map(entry=> ({...entry, distance: getDistance(currentLocation, {latitude:entry.latitude || 0, longitude: entry.longitude || 0}) * 0.000621371 }))
-			.sort((a,b) => a.distance > b.distance ? 1 : -1))
-		setLoading(false)
-	}, [currentLocation])
+    useEffect(() => {
+        setLoading(true);
+        setData((data) =>
+            data
+                .filter((entry) => entry.latitude && entry.longitude)
+                .map((entry) => ({
+                    ...entry,
+                    distance:
+                        getDistance(currentLocation, {
+                            latitude: entry.latitude || 0,
+                            longitude: entry.longitude || 0,
+                        }) * 0.000621371,
+                }))
+                .sort((a, b) => (a.distance > b.distance ? 1 : -1))
+        );
+        setLoading(false);
+    }, [currentLocation]);
 
-	const resetData = () => {
-		fetch("https://mzqsa4noec.execute-api.us-east-1.amazonaws.com/prod")
-			.then(async (res) => {
-				const newData = await res.json();
-				setData(JSON.parse(newData.body).results);
-				setReady(true);
-			})
-			.catch((ex) => {
-				console.error(ex.message);
-				setErrorMessage(
-					"something went wrong, please try again later."
-				);
-				setReady(true);
-			});
-	}
+    const resetData = () => {
+        fetch("https://mzqsa4noec.execute-api.us-east-1.amazonaws.com/prod")
+            .then(async (res) => {
+                const newData = await res.json();
+                setData(JSON.parse(newData.body).results);
+                setReady(true);
+            })
+            .catch((ex) => {
+                console.error(ex.message);
+                setErrorMessage(
+                    "something went wrong, please try again later."
+                );
+                setReady(true);
+            });
+    };
 
     const formattedData = sortAndFilterData(
         transformData(data),
@@ -122,25 +130,29 @@ export default function CovidAppointmentTable() {
         onlyShowAvailable
     );
 
-const getCurrentLocation = () => {
-	setZipcode("")
-	navigator.geolocation.getCurrentPosition((position) => {
-	  setCurrentLocation({latitude: position.coords.latitude, longitude: position.coords.longitude});
-	  })
-}
+    const getCurrentLocation = () => {
+        setZipcode("");
+        navigator.geolocation.getCurrentPosition((position) => {
+            setCurrentLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            });
+        });
+    };
 
-const getZipcodeLocation = () => {
-	fetch(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${zipcode}&facet=state&facet=timezone&facet=dst`).then(
-		async (res) => {
-			const {records} = await res.json();
-			const {fields: {latitude, longitude}} = records[0]
-			setCurrentLocation({latitude, longitude})
-		}
-	);
-}    
+    const getZipcodeLocation = () => {
+        fetch(
+            `https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${zipcode}&facet=state&facet=timezone&facet=dst`
+        ).then(async (res) => {
+            const { records } = await res.json();
+            const {
+                fields: { latitude, longitude },
+            } = records[0];
+            setCurrentLocation({ latitude, longitude });
+        });
+    };
 
-
-return (
+    return (
         <>
             <div
                 aria-label="loading data"
@@ -151,11 +163,32 @@ return (
                 <Loader loaded={ready} />
             </div>
 
-			<div >
-				<TextField id="outlined-basic" label="Enter Zipcode" variant="outlined" onChange={e => setZipcode(e.target.value)} value={zipcode}/>
-				<Button className={classes.filterButtons} variant="contained" color="secondary" onClick={getZipcodeLocation}>Search By Zip</Button>
-				<Button className={classes.filterButtons} variant="contained" color="secondary" startIcon={<GpsFixedIcon/>} onClick={getCurrentLocation}>Use My Location</Button>
-			</div>
+            <div>
+                <TextField
+                    id="outlined-basic"
+                    label="Enter Zipcode"
+                    variant="outlined"
+                    onChange={(e) => setZipcode(e.target.value)}
+                    value={zipcode}
+                />
+                <Button
+                    className={classes.filterButtons}
+                    variant="contained"
+                    color="secondary"
+                    onClick={getZipcodeLocation}
+                >
+                    Search By Zip
+                </Button>
+                <Button
+                    className={classes.filterButtons}
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<GpsFixedIcon />}
+                    onClick={getCurrentLocation}
+                >
+                    Use My Location
+                </Button>
+            </div>
 
             {errorMessage && <div role="alert">{errorMessage}</div>}
 
