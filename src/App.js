b49fa2ca4f2c85732e9789a93d6f2c28e652aad9
Linus Marco <lmarco163@gmail.com>
2021-02-17 22:16:15 -0500

    get filters into responsive drawer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loader";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Menu from "./components/Menu";
-import { makeStyles, MuiThemeProvider } from "@material-ui/core";
-import theme from "./theme";
-import StateEligibility from "./components/StateEligibility";
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import { Button, makeStyles, MuiThemeProvider } from "@material-ui/core";
 
+import theme from "./theme";
 import CovidAppointmentTable from "./CovidAppointmentTable";
+import Menu from "./components/Menu";
+import StateEligibility from "./components/StateEligibility";
 import FilterPanel from "./components/FilterPanel";
 import {
     filterData,
     getAppointmentData,
 } from "./services/appointmentData.service";
+
+
+const drawerWidth = 300;
 
 const useStyles = makeStyles((theme) => ({
     main: {
@@ -20,6 +25,38 @@ const useStyles = makeStyles((theme) => ({
     },
     heading: {
         "text-align": "center",
+    },
+    drawer: {
+        [theme.breakpoints.up('md')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        [theme.breakpoints.up('md')]: {
+            top: '70px',
+            height: 'calc(100% - 70px)',
+            border: 'none'
+        },
+        [theme.breakpoints.down('sm')]: {
+            top: 0,
+            height: '100% ',
+            borderRight: '1px solid rgba(0, 0, 0, 0.12)'
+        },
+
+    },
+    drawerMobile: {
+
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        [theme.breakpoints.up('md')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            borderLeft: '1px solid rgba(0, 0, 0, 0.12)'
+        },
     },
 }));
 
@@ -30,6 +67,7 @@ function App() {
     const [ready, setReady] = useState(false);
     const [errorMessage, setErrorMessage] = useState();
     const [filters, setFilters] = useState({});
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
     useEffect(() => {
         getAppointmentData()
@@ -48,23 +86,61 @@ function App() {
 
     const filteredData = filterData(data, filters);
 
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const mainContainer = document.getElementById('main-container');
+
     return (
         <MuiThemeProvider theme={theme}>
             <Menu />
             <main className={classes.main}>
                 <Grid container justify="center" spacing={3}>
-                    <Grid container={true}>
-                        <Grid item xs={false} md={3}></Grid>
-                        <Grid item xs={12} md={9}>
+                    <Grid container id="main-container">
+                        <Hidden mdUp implementation="css">
+                            <Drawer
+                                container={mainContainer}
+                                variant="temporary"
+                                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                                open={mobileOpen}
+                                onClose={handleDrawerToggle}
+                                classes={{
+                                    paper: classes.drawerPaper
+                                }}
+                                className={classes.drawerMobile}
+                                ModalProps={{
+                                    keepMounted: true, // Better open performance on mobile.
+                                }}
+                            >
+                                <FilterPanel data={data} onChange={setFilters} />
+                            </Drawer>
+                        </Hidden>
+
+                        <Hidden smDown implementation="css">
+                            <Drawer
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                                variant="permanent"
+                                open
+                            >
+                                <FilterPanel data={data} onChange={setFilters} />
+                            </Drawer>
+                        </Hidden>
+                        <Grid className={classes.content}>
                             <h1 className={classes.heading}>
                                 MA Covid Vaccine Appointments
                             </h1>
-                        </Grid>
-                        <Grid item xs={12} md={3}>
-                            <FilterPanel data={data} onChange={setFilters} />
-                        </Grid>
-                        <Grid item xs={12} md={9}>
                             <StateEligibility />
+                            <Hidden mdUp implementation="css">
+                                <Button
+                                    onClick={handleDrawerToggle}
+                                >
+                                    {/* TODO THIS IS UGLY */}
+                                    {'<<< '}Filter Sites
+                            </Button>
+                            </Hidden>
                             <div
                                 aria-label="loading data"
                                 id="progress"
