33f3bc331aba8d8af54511ec87a9717cbca4cb5e
Linus Marco <linusmarco@users.noreply.github.com>
2021-02-18 09:56:38 -0500

    Put filters into a responsive drawer (#42)
    * Update docs to make PRs to develop
    
    * update develop again (#32)
    
    * Improve HTML semantics of a couple key elements
    
    * Tweaks to elements
    
    * Add eligibility accordion
    
    * Remove extraneous prettierrc and prettify README.md
    
    * Link to mass.gov/covidvaccine gave a security warning becuase the state didn't redirect properly. Link changed to a non-redirecting link.
    
    * Update eligibility for feb 18, small refactor
    
    * link to senior housing details
    
    Co-authored-by: Matthew Cardarelli <mattrc7@gmx.com>
    Co-authored-by: Jake Rogers <jake@dataplusmath.com>
    Co-authored-by: Rob Harris <rob@harcod.com>
    Co-authored-by: John Hawkinson <jhawk@alum.mit.edu>
    
    * get filters into responsive drawer
    
    Co-authored-by: Olivia Adams <liv.gust@gmail.com>
    Co-authored-by: Matthew Cardarelli <mattrc7@gmx.com>
    Co-authored-by: Jake Rogers <jake@dataplusmath.com>
    Co-authored-by: Rob Harris <rob@harcod.com>
    Co-authored-by: John Hawkinson <jhawk@alum.mit.edu>

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loader";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Menu from "./components/Menu";
-import { makeStyles, MuiThemeProvider } from "@material-ui/core";
-import theme from "./theme";
-import StateEligibility from "./components/StateEligibility";
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import { Button, makeStyles, MuiThemeProvider } from "@material-ui/core";
 
+import theme from "./theme";
 import CovidAppointmentTable from "./CovidAppointmentTable";
+import Menu from "./components/Menu";
+import StateEligibility from "./components/StateEligibility";
 import FilterPanel from "./components/FilterPanel";
 import {
     filterData,
     getAppointmentData,
 } from "./services/appointmentData.service";
+
+
+const drawerWidth = 300;
 
 const useStyles = makeStyles((theme) => ({
     main: {
@@ -20,6 +25,38 @@ const useStyles = makeStyles((theme) => ({
     },
     heading: {
         "text-align": "center",
+    },
+    drawer: {
+        [theme.breakpoints.up('md')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        [theme.breakpoints.up('md')]: {
+            top: '70px',
+            height: 'calc(100% - 70px)',
+            border: 'none'
+        },
+        [theme.breakpoints.down('sm')]: {
+            top: 0,
+            height: '100% ',
+            borderRight: '1px solid rgba(0, 0, 0, 0.12)'
+        },
+
+    },
+    drawerMobile: {
+
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        [theme.breakpoints.up('md')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+            marginLeft: drawerWidth,
+            borderLeft: '1px solid rgba(0, 0, 0, 0.12)'
+        },
     },
 }));
 
@@ -30,6 +67,7 @@ function App() {
     const [ready, setReady] = useState(false);
     const [errorMessage, setErrorMessage] = useState();
     const [filters, setFilters] = useState({});
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
     useEffect(() => {
         getAppointmentData()
@@ -48,23 +86,61 @@ function App() {
 
     const filteredData = filterData(data, filters);
 
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const mainContainer = document.getElementById('main-container');
+
     return (
         <MuiThemeProvider theme={theme}>
             <Menu />
             <main className={classes.main}>
                 <Grid container justify="center" spacing={3}>
-                    <Grid container={true}>
-                        <Grid item xs={false} md={3}></Grid>
-                        <Grid item xs={12} md={9}>
+                    <Grid container id="main-container">
+                        <Hidden mdUp implementation="css">
+                            <Drawer
+                                container={mainContainer}
+                                variant="temporary"
+                                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                                open={mobileOpen}
+                                onClose={handleDrawerToggle}
+                                classes={{
+                                    paper: classes.drawerPaper
+                                }}
+                                className={classes.drawerMobile}
+                                ModalProps={{
+                                    keepMounted: true, // Better open performance on mobile.
+                                }}
+                            >
+                                <FilterPanel data={data} onChange={setFilters} />
+                            </Drawer>
+                        </Hidden>
+
+                        <Hidden smDown implementation="css">
+                            <Drawer
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                                variant="permanent"
+                                open
+                            >
+                                <FilterPanel data={data} onChange={setFilters} />
+                            </Drawer>
+                        </Hidden>
+                        <Grid className={classes.content}>
                             <h1 className={classes.heading}>
                                 MA Covid Vaccine Appointments
                             </h1>
-                        </Grid>
-                        <Grid item xs={12} md={3}>
-                            <FilterPanel data={data} onChange={setFilters} />
-                        </Grid>
-                        <Grid item xs={12} md={9}>
                             <StateEligibility />
+                            <Hidden mdUp implementation="css">
+                                <Button
+                                    onClick={handleDrawerToggle}
+                                >
+                                    {/* TODO THIS IS UGLY */}
+                                    {'<<< '}Filter Sites
+                            </Button>
+                            </Hidden>
                             <div
                                 aria-label="loading data"
                                 id="progress"
@@ -75,10 +151,10 @@ function App() {
                                     {errorMessage ? (
                                         <div role="alert">{errorMessage}</div>
                                     ) : (
-                                        <CovidAppointmentTable
-                                            data={filteredData}
-                                        />
-                                    )}
+                                            <CovidAppointmentTable
+                                                data={filteredData}
+                                            />
+                                        )}
                                 </Loader>
                             </div>
                             <Typography
@@ -86,15 +162,15 @@ function App() {
                                 display="block"
                                 gutterBottom
                             >
-                                This site is not affiliated with or endorsed by
-                                the Commonwealth of Massachusetts.
+                                This site is not affiliated with or endorsed by the
+                                Commonwealth of Massachusetts.
                                 <br />
-                                This site is for informational purposes only.
-                                Not all vaccination locations are tracked and
-                                the information may not be complete or accurate.
+                                This site is for informational purposes only. Not
+                                all vaccination locations are tracked and the
+                                information may not be complete or accurate.
                                 <br />
-                                Copyright &#169; {new Date().getFullYear()}{" "}
-                                Olivia Adams. All rights reserved.
+                                Copyright &#169; {new Date().getFullYear()} Olivia
+                                Adams/Ora Innovations LLC. All rights reserved.
                             </Typography>
                         </Grid>
                     </Grid>
