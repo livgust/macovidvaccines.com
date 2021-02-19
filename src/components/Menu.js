c4ffc7e11f506ecfd5c61465c19124d38d3cff2d
Bernard Farrell <bernard.farrell+github@gmail.com>
2021-02-19 08:18:04 -0500

    Add github and Slack links to About dialog
    Add links to github repositories and Slack.
    #22

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,15 +2,15 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -153,6 +153,36 @@ function AboutDialog(props) {
                             send an email
                         </a>
                         .
+                    </p>
+                    <h3>Get Involved</h3>
+                    <p>If you would like to help in the design or development of this website, read on.</p>
+                    <p>
+                        You can connect with the team on our{" "}
+                        <a
+                            href={"https://macovidvaccines.slack.com/"}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Slack workspace
+                        </a>.
+                    </p>
+                    <p>
+                        The website source code can be found on Github. The code that gathers data from
+                        other sites is in the{" "}
+                        <a
+                            href={"https://github.com/livgust/covid-vaccine-scrapers"}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            covid-vaccine-scrapers
+                        </a> repository. The code that generates the website is in the{" "}
+                        <a
+                            href={"https://github.com/livgust/macovidvaccines.com"}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            macovidvaccines.com
+                        </a> repository.
                     </p>
                 </DialogContentText>
             </DialogContent>
