b0087d0cd0d9bf688ba6b059a6c851b9b83b4698
Rob Harris <harcod@users.noreply.github.com>
2021-02-21 13:22:46 -0500

    Filters out old data (#68)
    * Issue#60
    Filters out any data that is older than "tooStaleMinutes" which is currently set at "60" (1 hour)
    
    * Chained the filters for readability.
    Moved constant to top of the file.
    Added comments for maintainability.
    
    * changes to increase clarity and performance

--- src/CovidAppointmentTable.js
+++ src/CovidAppointmentTable.js
@@ -16,6 +16,9 @@ import StaleDataIndicator from "./components/StaleDataIndicator";
 import Switch from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
 
+// any location with data older than this will not be displayed at all
+export const tooStaleMinutes = 60;  // unit in minutes
+
 export function transformData(data) {
     return data.map((entry, index) => {
         return {
@@ -39,10 +42,17 @@ export function sortAndFilterData(
     { sortKey, sortAsc },
     onlyShowAvailable
 ) {
-    const filteredData = onlyShowAvailable
-        ? data.filter((entry) => hasSignUpLink(entry))
-        : data;
-    const newData = filteredData.sort((a, b) => {
+    // Filter the locations that have "non-stale" data
+    const oldestGoodTimestamp = new Date() - (tooStaleMinutes * 60 * 1000);
+    let filteredData = data.filter(({ timestamp }) => !timestamp || timestamp >= oldestGoodTimestamp);
+
+    // Filter only the locations that have a sign up link, if desired
+    if (onlyShowAvailable) {
+        filteredData = filteredData.filter((entry) => hasSignUpLink(entry));
+    }
+
+    // Sort the data
+    return filteredData.sort((a, b) => {
         const first = sortAsc ? a[sortKey] : b[sortKey];
         const second = sortAsc ? b[sortKey] : a[sortKey];
         if (typeof first == "string") {
@@ -51,7 +61,6 @@ export function sortAndFilterData(
             return first - second;
         }
     });
-    return newData;
 }
 
 const useStyles = makeStyles((theme) => ({
