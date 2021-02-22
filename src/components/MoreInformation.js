cf542ebbc07d90d8ac7f1aafe301bbfc3ed7d546
Thomas Ricci <79022795+tom-ricci@users.noreply.github.com>
2021-02-22 12:37:29 -0500

    Fixed duplicate Additional Information (#77)
    * added formatting
    
    * format
    
    * fixed pr comments
    
    * finished markdown parsing
    
    * removed redundant style
    
    * Sanitize generated HTML
    
    * Fix sanitize config
    
    * fixed other comments
    
    * formatted document
    
    * Fix bug and remove color class from strong tag
    
    * fixed double additional information bug
    
    * removed unused argument
    
    * formatted document
    
    Co-authored-by: Matthew Cardarelli <mattrc7@gmx.com>

--- src/components/MoreInformation.js
+++ src/components/MoreInformation.js
@@ -69,7 +69,7 @@ function ExtraData({ data }) {
             if (key === "Additional Information") {
                 const currentData = data[key];
                 if (currentData) {
-                    let finalData = parseMD(currentData, classes);
+                    let finalData = parseMD(currentData);
                     //adds the parsed data as straight html
                     elements.push(
                         <div
@@ -92,14 +92,17 @@ function ExtraData({ data }) {
     }
 }
 
-function parseMD(currentData, classes) {
-    //encodes angle brackets to escape data
-    /** not sure if we still need this with the sanitizer so im commenting it out
-     *  let workingData1 = currentData.replaceAll("<", "&#60;");
-     *  let workingData2 = workingData1.replaceAll(">", "&#62;");
-     */
+function parseMD(currentData) {
+    //replaces first instance of additional information if it's at the beginning of the string
+    let splitAddInfo = currentData.split("Additional Information:");
+    let workingData = "";
+    if (splitAddInfo[0] === "") {
+        workingData = currentData.replace("Additional Information:", "");
+    } else {
+        workingData = currentData;
+    }
     //parses bold italics first, splitting data at each "***"
-    const splitDataEmStr = /*workingData2*/ currentData.split("***");
+    const splitDataEmStr = workingData.split("***");
     let newDataEmStr = splitDataEmStr[0];
     let isEmStr = false;
     //iterates through the split data
@@ -116,7 +119,6 @@ function parseMD(currentData, classes) {
     }
     //parses bold next, splitting data at each "**"
     const splitDataStr = newDataEmStr.split("**");
-    console.log(splitDataStr);
     let newDataStr = splitDataStr[0];
     let isStr = false;
     //iterates through the split data
