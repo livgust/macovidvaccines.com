6c1f3dae99b9731d67dbdb7947f840f3ceaf99bb
Rob Harris <harcod@users.noreply.github.com>
2021-02-28 20:52:54 -0500

    Hide availability when no sign up link (#85)
    * Update docs to make PRs to develop
    
    * Added Material-UI Alert system
    
    * added whitespace below Alert for better visual
    
    * Added a test harness to develop front-end with old cached files.
    Doesn't run in "production" mode.  Looks for a file called "test/devtest.json" and uses that instead of the AWS fetched "prod" file.
    
    * No longer shows date-specific slots if there is no sign up link for that date.
    
    * Updated testing branch to accept either cached timestamped json files from AWS, or "View Source" from a Browser.
    
    * Rollback testing harness
    
    * small coding style change
    
    Co-authored-by: Olivia Adams <liv.gust@gmail.com>

--- src/components/Availability.js
+++ src/components/Availability.js
@@ -1,6 +1,6 @@
 import HelpDialog from "./HelpDialog";
 
-export default function Availability({ entry }) {
+export default function Availability({entry, onlyShowAvailable}) {
     if (!entry.hasAppointments) {
         return <div>No availability.</div>;
     } else if (
@@ -9,14 +9,22 @@ export default function Availability({ entry }) {
     ) {
         return (
             <div>
-                {entry.totalAvailability} slot
-                {entry.totalAvailability > 1 ? "s" : ""}
+                {entry.totalAvailability}
+                {entry.totalAvailability === 1 ? "slot" : "slots"}
             </div>
         );
     } else {
         const availableSlots = [];
+        const singleSignupLink = (!!entry.signUpLink);
         for (const date in entry.appointmentData) {
-            if (entry.appointmentData[date].hasAvailability) {
+
+            // Show dates that have availability AND one of these three conditions:
+            // (1) We are showing all appointments; OR
+            // (2) The site has a single signup link; OR
+            // (3) This date has it's own signup link
+            if (entry.appointmentData[date].hasAvailability &&
+                (!onlyShowAvailable || singleSignupLink || entry.appointmentData[date].signUpLink)) {
+
                 availableSlots.push({
                     date: date,
                     ...entry.appointmentData[date],
