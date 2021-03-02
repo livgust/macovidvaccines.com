1b4697e139ad7d12f353d7e3acab7a3514ebd77e
Rob Harris <rob@harcod.com>
2021-03-02 17:25:44 -0500

    updating develop branch to match master PR #96

--- src/services/appointmentData.service.js
+++ src/services/appointmentData.service.js
@@ -1,8 +1,21 @@
+const dayjs = require("dayjs");
+
 // any location with data older than this will not be displayed at all
-const tooStaleMinutes = 60; // unit in minutes
+export const tooStaleMinutes = 6000; // unit in minutes
 
 export function transformData(data) {
+    const ourDateFormat = "M/D/YY"; // 3/2
+    // future format?    "ddd, MMM D"; // Tue Mar 2
+
     let mappedData = data.map((entry, index) => {
+        let availability = [];
+        if (entry.availability) {
+            for (const [key, value] of Object.entries(entry.availability)) {
+                let newKey = dayjs(key).format(ourDateFormat);
+                availability[newKey] = value;
+            }
+        }
+
         return {
             key: index,
             location: entry.name,
@@ -10,9 +23,10 @@ export function transformData(data) {
             city: entry.city,
             zip: entry.zip,
             hasAppointments: entry.hasAvailability,
-            appointmentData: entry.availability || null,
+            appointmentData: availability || null,
             signUpLink: entry.signUpLink || null,
             extraData: entry.extraData || null,
+            restrictions: entry.restrictions || null,
             timestamp: entry.timestamp ? new Date(entry.timestamp) : null,
         };
     });
