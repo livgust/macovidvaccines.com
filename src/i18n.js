6919612a91176ff2a2f7daf8764136b974777653
Matthew Cardarelli <mattrc7@gmx.com>
2021-03-03 20:05:38 -0500

    Create translations file structure

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import translations_en_main from "./translations/translations.en.main.json";
+
 i18n.use(initReactI18next).init({
     fallbackLng: "en-US",
     interpolation: { escapeValue: false }, // React performs escaping.
     resources: {
         en: {
-            main: {
-                page_title: "MA Covid Vaccine Appointments",
-            },
+            main: translations_en_main,
         },
     },
 });
