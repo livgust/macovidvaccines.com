6f3f931c6bac439f4e5d19a9dfab0e8e1c959002
John Hawkinson <jhawk@alum.mit.edu>
2021-03-06 17:24:37 -0500

    generate-ma.zips.js: Prefer /* * * */ for multiline comments

--- scripts/generate-ma-zips.js
+++ scripts/generate-ma-zips.js
@@ -1,28 +1,33 @@
-// Generate a pared down list of Massachusetts zipcodes with their
-// longitude and latitude.
+/*
+ * Generate a pared down list of Massachusetts zipcodes with their
+ * longitude and latitude.
+ *
+ */
 
 const fs = require("fs");
 const zipcodes = require("zipcodes-perogi");
 
 const MAZipcodes = zipcodes.lookupByState("MA");
 
-// 'zipcodes-perogi' gives us this form, which is more than we need:
-// [ ... {
-//   zip: '02130',
-//   city: 'Jamaica Plain',
-//   state: 'MA',
-//   latitude: '42.29472',
-//   longitude: '-71.13054',
-//   timeZoneId: 'America/New_York'
-// } ... ]
-//
-// Previously we were using 'uszips', which gives us:
-// { ... {
-//   '02130': {
-//     latitude: '42.29472',
-//     longitude: '-71.13054'
-// } ... }
-//
+/*
+ * 'zipcodes-perogi' gives us this form, which is more than we need:
+ * [ ... {
+ *   zip: '02130',
+ *   city: 'Jamaica Plain',
+ *   state: 'MA',
+ *   latitude: '42.29472',
+ *   longitude: '-71.13054',
+ *   timeZoneId: 'America/New_York'
+ * } ... ]
+ *
+ * Previously we were using 'uszips', which gives us:
+ * { ... {
+ *   '02130': {
+ *     latitude: '42.29472',
+ *     longitude: '-71.13054'
+ * } ... }
+ *
+ */
 
 const MAZipcodesSmaller = Object.fromEntries(
     MAZipcodes.map((z) => [
