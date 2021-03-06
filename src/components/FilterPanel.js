bb30617f7bf7c93a5c0e0ecf8fc6c88765dd3819
Rob Harris <harcod@users.noreply.github.com>
2021-03-06 17:14:10 -0500

    Invalid (or Unknown) ZIP Codes sort by location. (#120)
    * Invalid (or Unknown) ZIP Codes sort by location.
    
    * Use conversion function from geolib

--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -13,7 +13,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { hasSignUpLink } from "./SignUpLink";
-import { getDistance } from "geolib";
+import { convertDistance, getDistance } from "geolib";
 import { setSortBy } from "../services/appointmentData.service";
 import Cookies from "universal-cookie";
 
@@ -221,23 +221,29 @@ export default function FilterPanel(props) {
                 if (d) {
                     const zipValid = zipCodeFilter.zipCode.match(/\d{5}/);
                     if (zipValid) {
-                        setSortBy("miles");
                         const myCoordinates = usZips[zipCodeFilter.zipCode];
                         if (myCoordinates) {
-                            const metersPerMile = 1609.34;
+                            setSortBy("miles");
                             d.miles = Math.round(
-                                getDistance(myCoordinates, d.coordinates, 1) /
-                                    metersPerMile
+                                convertDistance(
+                                    getDistance(
+                                        myCoordinates,
+                                        d.coordinates,
+                                        1
+                                    ),
+                                    "mi"
+                                )
                             );
 
                             // Is the location within the range specified?
-                            return d.miles < zipCodeFilter.miles;
+                            return d.miles <= zipCodeFilter.miles;
                         }
-                    } else {
-                        setSortBy("location");
                     }
                 }
-                return true; // include all for now.
+                // No zipcode was provided or
+                // was unable to find coordinates for the specified zipcode
+                setSortBy("location");
+                return true;
             },
             /*
             vaxType: (d) => {
