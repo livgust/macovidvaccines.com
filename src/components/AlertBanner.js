7cec112eadb6a09298b5ac25999198f772bb1eec
Rob Harris <rob@harcod.com>
2021-03-07 13:32:07 -0500

    added filter and comments for readability

--- src/components/AlertBanner.js
+++ src/components/AlertBanner.js
@@ -9,30 +9,52 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+/* There are four severity levels each has its own icon built-in
+ *  error   - red
+ *  warning - orange (** this is what you should probably use)
+ *  info    - blue
+ *  success - green
+ */
+/* The "-05:00" at the end of the dates adjust from EST to UTC
+ * NOTE: DST begins on 3/14/2021 when the offset becomes "-04:00"
+ * NOTE: EST begins on 11/7/2021 when the offset becomes "-05:00"
+ */
 const alerts = [
-    /*    {
-        startDate: "2021-03-04T06:00:00-05:00",
-        endDate: "2021-03-04T09:00:00-05:00",
-        severity: "warning",
+    /*
+    {
+        startDate: "2021-03-04T06:00:00-05:00", // current timezone offset is at the end
+        endDate: "2021-03-24T09:00:00-05:00",
+        severity: "warning", // one of "error", "info", "success", "warning"
         title: "Thursday, March 4",
         contents:
             "Due to high demand, the MA vaccination websites are experiencing technical difficulties. Once the issues are resolved, their locations will appear on this website.",
-    },*/
+    },
+*/
 ];
 
 export default function AlertBanner() {
     const classes = useStyles();
 
+    // No alerts? Then we're done!
+    if (!alerts.length) {
+        return false;
+    }
+
+    // Filter all of the timely alerts
     const now = new Date();
-    return alerts.map((alert) => {
-        return new Date(alert.startDate) <= now &&
-            now < new Date(alert.endDate) ? (
+    const timelyAlerts = alerts.filter((alert) => {
+        return (
+            new Date(alert.startDate) <= now && now < new Date(alert.endDate)
+        );
+    });
+
+    // Output an Alert for each of the remaining timely alerts
+    return timelyAlerts.map((alert) => {
+        return (
             <Alert severity={alert.severity} className={classes.alertBanner}>
                 <AlertTitle>{alert.title}</AlertTitle>
                 {alert.contents}
             </Alert>
-        ) : (
-            <></>
         );
     });
 }
