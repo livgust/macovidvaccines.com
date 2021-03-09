52a2673066d9fc5c11a20df0ee3e62b3eb4c3e23
Olivia Adams <liv.gust@gmail.com>
2021-03-09 15:40:15 -0500

    Update filter design (#131)
    * migrate filters to subcomponents
    
    * consolidate filtering
    
    * clean-up and re-introduce zip ordering
    
    * ma-zips: Include cityname
    
    It appears to be necessary for the redesigned filters (#3).
    Regen file.
    
    Note the naive gzip moves from 8k to 12k.
    (bzip2 moves from 6k to 8k).
    
    pb3:generated jhawk$ ls -lR
    total 176
    -rw-r--r--  1 jhawk  staff  52257 Mar  7 16:25 ma-zips.json
    -rw-r--r--  1 jhawk  staff  11286 Mar  7 16:25 ma-zips.json.9gz
    -rw-r--r--  1 jhawk  staff   8726 Mar  7 16:25 ma-zips.json.bz2
    -rw-r--r--  1 jhawk  staff  11576 Mar  7 16:25 ma-zips.json.gz
    
    ./nocity:
    total 128
    -rw-r--r--  1 jhawk  staff  39207 Mar  7 16:24 ma-zips.json
    -rw-r--r--  1 jhawk  staff   7751 Mar  6 16:02 ma-zips.json.9gz
    -rw-r--r--  1 jhawk  staff   6363 Mar  6 16:02 ma-zips.json.bz2
    -rw-r--r--  1 jhawk  staff   8114 Mar  6 16:02 ma-zips.json.gz
    
    * fix name sorting
    
    * Revert "ma-zips: Include cityname"
    
    This reverts commit dfd92a312cd2e932b8f85705e22bfd5938d24585.
    
    Co-authored-by: John Hawkinson <jhawk@alum.mit.edu>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 /*eslint no-unused-vars: ["error", { "varsIgnorePattern": "[iI]gnored" }]*/
 
-import Alert from "@material-ui/lab/Alert";
-import AlertTitle from "@material-ui/lab/AlertTitle";
-import AlertBanner from "./components/AlertBanner";
-import { Button, makeStyles, MuiThemeProvider } from "@material-ui/core";
-import CovidAppointmentTable from "./CovidAppointmentTable";
-import Drawer from "@material-ui/core/Drawer";
-import FilterListIcon from "@material-ui/icons/FilterList";
-import FilterPanel, { getZipCodeCookie } from "./components/FilterPanel";
+import {
+    createMuiTheme,
+    makeStyles,
+    MuiThemeProvider,
+} from "@material-ui/core";
 import {
     filterData,
     getAppointmentData,
 } from "./services/appointmentData.service";
+import {
+    getZipCodeCookie,
+    isZipValid,
+} from "./components/FilterPanel/ZipCodeFilter";
+import { setSortBy } from "./services/appointmentData.service";
+import Alert from "@material-ui/lab/Alert";
+import AlertBanner from "./components/AlertBanner";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import Button from "@material-ui/core/Button";
+import CovidAppointmentTable from "./CovidAppointmentTable";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import FilterPanel from "./components/FilterPanel";
 import Grid from "@material-ui/core/Grid";
 import Hidden from "@material-ui/core/Hidden";
 import Loader from "react-loader";
 import Menu from "./components/Menu";
 import React, { useEffect, useState } from "react";
-import Typography from "@material-ui/core/Typography";
-import theme from "./theme";
 import StateEligibility from "./components/StateEligibility";
+import Typography from "@material-ui/core/Typography";
+import themeTemplate from "./theme";
 
-const drawerWidth = 300;
+const theme = createMuiTheme(themeTemplate);
 
 const useStyles = makeStyles((theme) => ({
     main: {
@@ -30,50 +39,51 @@ const useStyles = makeStyles((theme) => ({
     heading: {
         "text-align": "center",
     },
-    drawer: {
-        [theme.breakpoints.up("md")]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        [theme.breakpoints.up("md")]: {
-            top: "70px",
-            height: "calc(100% - 70px)",
-            border: "none",
-        },
-        [theme.breakpoints.down("sm")]: {
-            top: 0,
-            height: "100% ",
-            borderRight: "1px solid rgba(0, 0, 0, 0.12)",
-        },
-    },
-    drawerMobile: {},
-    mobileButton: { width: "50%", marginLeft: theme.spacing(3) },
     content: {
         flexGrow: 1,
         padding: theme.spacing(3),
         [theme.breakpoints.up("md")]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
+            width: `calc(100% - ${theme.drawerWidth}px)`,
+            marginLeft: theme.drawerWidth,
             borderLeft: "1px solid rgba(0, 0, 0, 0.12)",
         },
     },
 }));
 
 function App() {
-    const classes = useStyles();
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Menu />
+            <MainComponent />
+        </MuiThemeProvider>
+    );
+}
 
+function ErrorMessageAlert({ message }) {
+    //const classes = useStyles();
+    return (
+        <>
+            <Alert severity={"error"}>
+                <AlertTitle>Unexpected Internal Error</AlertTitle>
+                <p>{message}</p>
+            </Alert>
+            <br />
+        </>
+    );
+}
+
+function MainComponent() {
+    const classes = useStyles();
+    const mainContainer = document.getElementById("main-container");
     const [data, setData] = useState([]);
     const [ready, setReady] = useState(false);
     const [errorMessage, setErrorMessage] = useState();
     const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    // State variables for the two FilterPanels so that both update together
-    const [filters, setFilters] = useState({});
-    const [onlyShowAvailable, setOnlyShowAvailable] = useState(true);
-    const [zipCode, setZipCode] = useState(getZipCodeCookie());
+    const [filters, setFilters] = useState({
+        filterByAvailable: true,
+        filterByZipCode: { zipCode: getZipCodeCookie(), miles: 9999 },
+    });
 
     useEffect(() => {
         getAppointmentData()
@@ -91,143 +101,80 @@ function App() {
             });
     }, []);
 
+    useEffect(() => {
+        isZipValid(filters.filterByZipCode.zipCode)
+            ? setSortBy("miles")
+            : setSortBy("name");
+    }, [filters]);
+
     const filteredData = filterData(data, filters);
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
 
-    const mainContainer = document.getElementById("main-container");
-
     return (
-        <MuiThemeProvider theme={theme}>
-            <Menu />
-            <main className={classes.main}>
-                <Grid container justify="center" spacing={3}>
-                    <Grid container id="main-container">
+        <main className={classes.main}>
+            <Grid container justify="center" spacing={3}>
+                <Grid container id="main-container">
+                    <FilterPanel
+                        mainContainer={mainContainer}
+                        anchor={theme.direction === "rtl" ? "right" : "left"}
+                        mobileOpen={mobileOpen}
+                        handleDrawerToggle={handleDrawerToggle}
+                        filters={filters}
+                        setFilters={setFilters}
+                    />
+                    <Grid className={classes.content}>
+                        <h1 className={classes.heading}>
+                            MA Covid Vaccine Appointments
+                        </h1>
+                        <AlertBanner />
+                        <StateEligibility />
                         <Hidden mdUp implementation="css">
-                            <Drawer
-                                container={mainContainer}
-                                variant="temporary"
-                                anchor={
-                                    theme.direction === "rtl" ? "right" : "left"
-                                }
-                                open={mobileOpen}
-                                onClose={handleDrawerToggle}
-                                classes={{
-                                    paper: classes.drawerPaper,
-                                }}
-                                className={classes.drawerMobile}
-                                ModalProps={{
-                                    keepMounted: true, // Better open performance on mobile.
-                                }}
+                            <Button
+                                variant="contained"
+                                startIcon={<FilterListIcon />}
+                                onClick={handleDrawerToggle}
                             >
-                                <FilterPanel
-                                    data={data}
-                                    onChange={setFilters}
-                                    onlyShowAvailable={onlyShowAvailable}
-                                    setOnlyShowAvailable={setOnlyShowAvailable}
-                                    zipCode={zipCode}
-                                    setZipCode={setZipCode}
-                                    closeButton={
-                                        <Button
-                                            variant="contained"
-                                            color="primary"
-                                            className={classes.mobileButton}
-                                            onClick={handleDrawerToggle}
-                                        >
-                                            Update List
-                                        </Button>
-                                    }
-                                />
-                            </Drawer>
+                                Filter Locations
+                            </Button>{" "}
                         </Hidden>
-
-                        <Hidden smDown implementation="css">
-                            <Drawer
-                                classes={{
-                                    paper: classes.drawerPaper,
-                                }}
-                                variant="permanent"
-                                open
-                            >
-                                <FilterPanel
-                                    data={data}
-                                    onChange={setFilters}
-                                    onlyShowAvailable={onlyShowAvailable}
-                                    setOnlyShowAvailable={setOnlyShowAvailable}
-                                    zipCode={zipCode}
-                                    setZipCode={setZipCode}
-                                />
-                            </Drawer>
-                        </Hidden>
-                        <Grid className={classes.content}>
-                            <h1 className={classes.heading}>
-                                MA Covid Vaccine Appointments
-                            </h1>
-                            <AlertBanner />
-                            <StateEligibility />
-                            <Hidden mdUp implementation="css">
-                                <Button
-                                    variant="contained"
-                                    startIcon={<FilterListIcon />}
-                                    onClick={handleDrawerToggle}
-                                >
-                                    Filter Locations
-                                </Button>{" "}
-                            </Hidden>
-                            <div
-                                aria-label="loading data"
-                                id="progress"
-                                role="progressbar"
-                                aria-valuetext={ready ? "loaded" : "waiting"}
-                            >
-                                <Loader loaded={ready}>
-                                    {errorMessage ? (
-                                        <ErrorMessageAlert
-                                            message={errorMessage}
-                                        />
-                                    ) : (
-                                        <CovidAppointmentTable
-                                            data={filteredData}
-                                        />
-                                    )}
-                                </Loader>
-                            </div>
-                            <Typography
-                                variant="caption"
-                                display="block"
-                                gutterBottom
-                            >
-                                This site is not affiliated with or endorsed by
-                                the Commonwealth of Massachusetts.
-                                <br />
-                                This site is for informational purposes only.
-                                Not all vaccination locations are tracked and
-                                the information may not be complete or accurate.
-                                <br />
-                                Copyright &#169; {new Date().getFullYear()}{" "}
-                                Olivia Adams/Ora Innovations LLC. All rights
-                                reserved.
-                            </Typography>
-                        </Grid>
+                        <div
+                            aria-label="loading data"
+                            id="progress"
+                            role="progressbar"
+                            aria-valuetext={ready ? "loaded" : "waiting"}
+                        >
+                            <Loader loaded={ready}>
+                                {errorMessage ? (
+                                    <ErrorMessageAlert message={errorMessage} />
+                                ) : (
+                                    <CovidAppointmentTable
+                                        data={filteredData}
+                                    />
+                                )}
+                            </Loader>
+                        </div>
+                        <Typography
+                            variant="caption"
+                            display="block"
+                            gutterBottom
+                        >
+                            This site is not affiliated with or endorsed by the
+                            Commonwealth of Massachusetts.
+                            <br />
+                            This site is for informational purposes only. Not
+                            all vaccination locations are tracked and the
+                            information may not be complete or accurate.
+                            <br />
+                            Copyright &#169; {new Date().getFullYear()} Olivia
+                            Adams/Ora Innovations LLC. All rights reserved.
+                        </Typography>
                     </Grid>
                 </Grid>
-            </main>
-        </MuiThemeProvider>
-    );
-}
-
-function ErrorMessageAlert({ message }) {
-    //const classes = useStyles();
-    return (
-        <>
-            <Alert severity={"error"}>
-                <AlertTitle>Unexpected Internal Error</AlertTitle>
-                <p>{message}</p>
-            </Alert>
-            <br />
-        </>
+            </Grid>
+        </main>
     );
 }
 
