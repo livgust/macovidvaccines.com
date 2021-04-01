62026ca614782f5e1e16735fa1d871e2f8456940
Anthony Salmeron <anthonysalmerondev@gmail.com>
2021-03-31 20:10:00 -0400

    undoing extra package require

--- src/components/FilterPanel/ZipCodeFilter.test.js
+++ src/components/FilterPanel/ZipCodeFilter.test.js
@@ -1,4 +1,4 @@
-import { act, prettyDOM, render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import ZipCodeFilter, { zipInputFilter } from "./ZipCodeFilter";
